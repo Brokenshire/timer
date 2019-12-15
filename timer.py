ab8d776d9f9a54ca9a78bd807ccf96e127920135
Jack <29889878+Brokenshire@users.noreply.github.com>
2019-12-15 22:43:44 +1300

    Update timer.py

--- timer.py
+++ timer.py
@@ -21,7 +21,8 @@ class Application(tk.Frame):
         self.build_interface()
 
     def build_interface(self):
-        self.clock = tk.Label(self, text="")
+        """The interface function."""
+        self.clock = tk.Label(self, text="00:00:00")
         self.clock.grid(row=1)
 
         self.power_button = tk.Button(self, text="Start", command=lambda: self.timer(3600))
@@ -30,25 +31,28 @@ class Application(tk.Frame):
         self.reset_button = tk.Button(self, text="Reset", state="disabled")
         self.reset_button.grid(row=2, column=1)
 
-        self.quit_button = tk.Button(self, text="Quit", command=self.quitcountdown)
+        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
         self.quit_button.grid(row=2, column=3)
 
     def timer(self, t):
+        """Calculates the time to be displayed"""
         while t:
             self.hours = t // 3600
             self.mins = (t // 60) % 60 
             self.secs = t % 60
-            timeformat = '{:02d}:{:02d}:{:02d}'.format(self.hours, self.mins, self.secs)
+            timeformat = "{:02d}:{:02d}:{:02d}".format(self.hours, self.mins, self.secs)
             self.clock.configure(text=timeformat)
             time.sleep(1)
             t -= 1
 
-    def quitcountdown(self):
+    def quit(self):
+        """Ask user if they want to close program. If yes closes, if no goes back to timer."""
         if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
             root.destroy()
    
 
 if __name__ == "__main__":
+    """Main loop which creates program."""
     root = tk.Tk()
     root.title("TIMER")
     Application(root).pack(side="top", fill="both", expand=True)
