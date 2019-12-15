31418b3c197ed3198ef01d67cd011649c05a3d4d
Jack <29889878+Brokenshire@users.noreply.github.com>
2019-12-15 19:23:42 +1300

    Creation of timer class

---
+++ timer.py
@@ -0,0 +1,55 @@
+"""
+Simple timer GUI application using tkinter.
+
+Author: Jack Brokenshire
+Date: 15/12/2019
+"""
+
+# Standard import
+import time
+
+# Third-party imports
+import tkinter as tk
+import tkinter.messagebox
+
+
+class Application(tk.Frame):
+    """"Simple timer application using tkinter."""
+    def __init__(self, master, *args, **kwargs):
+        tk.Frame.__init__(self, master, *args, **kwargs)
+        self.master = master
+        self.build_interface()
+
+    def build_interface(self):
+        self.clock = tk.Label(self, text="")
+        self.clock.grid(row=1)
+
+        self.power_button = tk.Button(self, text="Start", command=lambda: self.timer(3600))
+        self.power_button.grid(row=2, column=0)
+
+        self.reset_button = tk.Button(self, text="Reset", state="disabled")
+        self.reset_button.grid(row=2, column=1)
+
+        self.quit_button = tk.Button(self, text="Quit", command=self.quitcountdown)
+        self.quit_button.grid(row=2, column=3)
+
+    def timer(self, t):
+        while t:
+            self.hours = t // 3600
+            self.mins = (t // 60) % 60 
+            self.secs = t % 60
+            timeformat = '{:02d}:{:02d}:{:02d}'.format(self.hours, self.mins, self.secs)
+            self.clock.configure(text=timeformat)
+            time.sleep(1)
+            t -= 1
+
+    def quitcountdown(self):
+        if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
+            root.destroy()
+   
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("TIMER")
+    Application(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
