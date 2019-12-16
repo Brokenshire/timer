e99d74e83078f439bc0b99ac2b86b38b6ade59df
Jack <29889878+Brokenshire@users.noreply.github.com>
2019-12-16 21:35:22 +1300

    working timer program

--- timer.py
+++ timer.py
@@ -18,7 +18,8 @@ class Application(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
         self.master = master
-        self.t = 3600
+        self.running = False
+        self.time = 3600
         self.hours = 0
         self.mins = 0
         self.secs = 0
@@ -29,31 +30,45 @@ class Application(tk.Frame):
         self.clock = tk.Label(self)
         self.clock.grid(row=1)
 
-        self.power_button = tk.Button(self, text="Start", command=lambda: self.timer())
+        self.power_button = tk.Button(self, text="Start", command=lambda: self.start())
         self.power_button.grid(row=2, column=0)
+
+        self.stop_button = tk.Button(self, text="Stop", command=lambda: self.stop())
+        self.stop_button.grid(row=2, column=4)
 
         self.reset_button = tk.Button(self, text="Reset", command=lambda: self.reset())
         self.reset_button.grid(row=2, column=1)
 
-        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
+        self.quit_button = tk.Button(self, text="Quit", command=lambda: self.quit())
         self.quit_button.grid(row=2, column=3)
 
         root.bind("<Return>", lambda: self.timer(t=3600))
 
     def timer(self):
         """Calculates the time to be displayed"""
-        if self.t <= 0:
-            self.clock.configure(text="Time's up!")
-        else:
-            self.hours = self.t // 3600
-            self.mins = (self.t // 60) % 60 
-            self.secs = self.t % 60
-            self.clock.configure(text="{:02d}:{:02d}:{:02d}".format(self.hours, self.mins, self.secs))
-            self.t -= 1
-            self.after(1000, self.timer)
+        if self.running == True:
+            if self.time <= 0:
+                self.clock.configure(text="Time's up!")
+            else:
+                self.hours = self.time // 3600
+                self.mins = (self.time // 60) % 60 
+                self.secs = self.time % 60
+                self.clock.configure(text="{:02d}:{:02d}:{:02d}".format(self.hours, self.mins, self.secs))
+                self.time -= 1
+                self.after(1000, self.timer)
+
+    def start(self):
+        """Begins the timer"""
+        self.running = True
+        self.timer()
+
+    def stop(self):
+        """Stops the timer"""
+        self.running = False
 
     def reset(self):
         """Resets the timer to 0."""
+        self.running = False
         self.clock["text"] = "00:00:00"
 
     def quit(self):
